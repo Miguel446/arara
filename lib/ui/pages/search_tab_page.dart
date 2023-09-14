dc7ace594e6abf5b7a934f21bd1ffea1babc4a5d
José Azevedo <joseazevedo47@hotmail.com>
2023-09-14 20:48:51 -0300

    refactor: renomeia SearchTabPage para ShopCard e refatora para widget receber uma instância de shop como parâmetro

--- lib/ui/pages/search_tab_page.dart
+++ lib/ui/pages/search_tab_page.dart
@@ -1,9 +1,9 @@
-import 'package:arara/ui/widgets/search_list_item_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../shared/models/shop.dart';
 import '../../shared/repositories/shop_repository.dart';
+import '../widgets/shop_card.dart';
 
 class SearchTabPage extends StatefulWidget {
   final ShopType shopType;
@@ -22,95 +22,27 @@ class _SearchTabPageState extends State<SearchTabPage> {
     setState(() => isLoading = true);
 
     try {
-      shops = await GetIt.I<ShopRepository>().getShops();
+      shops = await GetIt.I<ShopRepository>().getShops(type: widget.shopType);
     } finally {
       setState(() => isLoading = false);
     }
   }
 
   @override
+  void initState() {
+    super.initState();
+    getShops();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView(
       physics: const NeverScrollableScrollPhysics(),
-      children: [
-        const SizedBox(
-          height: 20,
-        ),
-        ...cards(widget.shopType == ShopType.virtual),
-      ],
+      children: shops
+          .map(
+            (shop) => ShopCard(shop),
+          )
+          .toList(),
     );
-  }
-
-  List<Widget> cards(bool isOnline) {
-    return isOnline
-        ? [
-            SearchListItemCard.virtual(
-              imgPathList: const [
-                'assets/brecho.png',
-                'assets/brecho12.jpeg',
-                'assets/brecho13.jpeg'
-              ],
-              title: 'Pinguinho Brechó Space',
-              instagram: '@brecho_pinguinho',
-              phone: '(91) 98337-5857',
-              paymentMethods: 'Formas de pagamento: pix, débito e crédito',
-            ),
-            SearchListItemCard.virtual(
-              imgPathList: const [
-                'assets/brecho2.png',
-                'assets/brecho22.jpeg',
-                'assets/brecho23.jpeg'
-              ],
-              title: 'Brechó de Elite',
-              instagram: '@brecho_elite',
-              phone: '(91) 98337-5857',
-              paymentMethods: 'Formas de pagamento: pix e dinheiro',
-            ),
-            SearchListItemCard.virtual(
-              imgPathList: const [
-                'assets/brecho3.png',
-                'assets/brecho32.jpeg',
-                'assets/brecho33.jpeg'
-              ],
-              title: 'Brechó Stylus',
-              instagram: '@stylus_brecho',
-              phone: '(91) 3212-0413',
-              paymentMethods: 'Formas de pagamento: pix, débito e crédito',
-            ),
-          ]
-        : [
-            SearchListItemCard.physical(
-              imgPathList: const [
-                'assets/brecho.png',
-                'assets/brecho12.jpeg',
-                'assets/brecho13.jpeg'
-              ],
-              title: 'Pinguinho Brechó Space',
-              address: 'Rua Domingos Marreiros, 727 - Umarizal',
-              horary: 'Seg. a Sex. - 08:00 às 18:00',
-            ),
-            SearchListItemCard.physical(
-              imgPathList: const [
-                'assets/brecho2.png',
-                'assets/brecho22.jpeg',
-                'assets/brecho23.jpeg'
-              ],
-              title: 'Brechó de Elite',
-              address: 'Tv. 14 de Março, 1304 - Umarizal',
-              horary: 'Seg. a Sab. - 09:00 às 18:00',
-              phone: '(91) 98337-5857',
-            ),
-            SearchListItemCard.physical(
-              imgPathList: const [
-                'assets/brecho3.png',
-                'assets/brecho32.jpeg',
-                'assets/brecho33.jpeg'
-              ],
-              title: 'Brechó Stylus',
-              address: 'Tv. Padre Eutíquio, 218 - Campina',
-              horary: 'Seg. a Sab. - 08:00 às 18:00',
-              phone: '(91) 3212-0413',
-            ),
-          ];
   }
 }
---
+++ lib/ui/widgets/shop_card.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../../enums/color_enum.dart';
+import '../../shared/models/shop.dart';
+
+class ShopCard extends StatelessWidget {
+  final Shop shop;
+
+  const ShopCard(this.shop, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(20, 0, 20, 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (shop.imageUrls?.isNotEmpty == true)
+            ClipRRect(
+              borderRadius: BorderRadius.circular(24.0),
+              child: ImageSlideshow(
+                height: 200,
+                indicatorRadius: 4,
+                indicatorBackgroundColor: Colors.white70,
+                indicatorColor: ColorEnum.verde,
+                autoPlayInterval: 0,
+                children: shop.imageUrls!
+                    .map(
+                      (url) => Image.asset(
+                        url,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          Container(
+            margin: const EdgeInsets.only(top: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  shop.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                if (shop.averageRating != null) ...[
+                  const Icon(
+                    Icons.star,
+                    color: ColorEnum.verde,
+                    size: 17,
+                  ),
+                  Text(
+                    '${shop.averageRating}',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Row(
+              children: [
+                if (shop.type == ShopType.virtual)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4),
+                    child: Icon(
+                      MdiIcons.instagram,
+                      size: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                Text(
+                  shop.type == ShopType.virtual
+                      ? shop.instagram!
+                      : shop.address!,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(
+              shop.openingHours,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Text(
+              shop.phone ?? '-',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

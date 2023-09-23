e5ed9f5fba7802c059985135a5ef9e3ad45a8724
José Azevedo <joseazevedo47@hotmail.com>
2023-09-23 16:41:32 -0300

    feat: add ShopReviewsPage body

--- lib/pages/shop_reviews_page.dart
+++ lib/pages/shop_reviews_page.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ShopReviewsPage extends StatelessWidget {
+import '../config/theme.dart';
+import '../shared/models/shop.dart';
+import '../shared/providers/shop_provider.dart';
+import '../widgets/card/review_card.dart';
+import '../widgets/error_message.dart';
+
+class ShopReviewsPage extends ConsumerWidget {
   const ShopReviewsPage(this.shopId, {super.key, this.shop});
 
   final String shopId;
@@ -8,13 +15,37 @@ class ShopReviewsPage extends StatelessWidget {
   static const path = '/brecho/:id/avaliacoes';
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncShop = ref.watch(shopProvider(shopId));
+
+    return asyncShop.when(
+      data: (shop) => ShopReviewsPageBody(shop),
+      loading: () => const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      ),
+      error: (_, __) => const ErrorMessage(),
+    );
+  }
+}
+
+class ShopReviewsPageBody extends StatelessWidget {
+  const ShopReviewsPageBody(this.shop, {super.key});
+
+  final Shop shop;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          // title: Text(
-          //   shop!.name,
-          // ),
-          ),
+        title: Text(
+          shop.name,
+        ),
+      ),
+      body: ListView.builder(
+        padding: AppTheme.pagePadding.copyWith(top: 20),
+        itemCount: shop.reviews?.length ?? 0,
+        itemBuilder: (_, index) => ReviewCard(shop.reviews![index]),
+      ),
     );
   }
 }

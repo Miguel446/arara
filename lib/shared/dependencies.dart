483b418e93b5effb190a4cfb8781819f6550b861
José Azevedo <joseazevedo47@hotmail.com>
2023-09-24 19:36:31 -0300

    feat: add AuthRepository

--- lib/shared/dependencies.dart
+++ lib/shared/dependencies.dart
@@ -1,7 +1,9 @@
 import 'package:get_it/get_it.dart';
 
+import 'repositories/auth_repository.dart';
 import 'repositories/shop_repository.dart';
 
 void registerDependencies() {
+  GetIt.I.registerSingleton(AuthRepository());
   GetIt.I.registerSingleton(ShopRepository());
 }

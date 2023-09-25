2e01c22480432b9226b6bf3dd62d1d3ca4452b85
José Azevedo <joseazevedo47@hotmail.com>
2023-09-24 22:20:20 -0300

    fix: set initial location to home only if there is a stored user

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -4,8 +4,10 @@ import '../pages/home_page.dart';
 import '../pages/login_page.dart';
 import '../pages/shop_detail_page.dart';
 import '../pages/shop_reviews_page.dart';
+import '../shared/providers/user_provider.dart';
 
 final router = GoRouter(
+  initialLocation: UserNotifier.isUserSet ? HomePage.path : LoginPage.path,
   routes: [
     GoRoute(path: LoginPage.path, builder: (_, __) => const LoginPage()),
     GoRoute(path: HomePage.path, builder: (_, __) => const HomePage()),

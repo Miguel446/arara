4e79629e0ef4b0c61e2e73258c2cdfe6bce0ba65
José Azevedo <joseazevedo47@hotmail.com>
2023-10-08 21:17:20 -0300

    feat(AccountMenuItem): add onPressed parameter

--- lib/widgets/account_menu_item.dart
+++ lib/widgets/account_menu_item.dart
@@ -2,44 +2,52 @@ import 'package:arara/config/theme.dart';
 import 'package:flutter/material.dart';
 
 class AccountMenuItem extends StatelessWidget {
+  const AccountMenuItem({
+    super.key,
+    required this.title,
+    required this.iconData,
+    this.onPressed,
+  });
+
   final String title;
   final IconData iconData;
-
-  const AccountMenuItem(
-      {super.key, required this.title, required this.iconData});
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.grey,
-            width: 1,
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey,
+              width: 1,
+            ),
           ),
         ),
-      ),
-      padding: const EdgeInsets.only(bottom: 8, top: 16),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: Icon(
-              iconData,
-              size: 22,
+        padding: const EdgeInsets.only(bottom: 8, top: 16),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Icon(
+                iconData,
+                size: 22,
+              ),
             ),
-          ),
-          Expanded(
-            child: Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.w500),
+            Expanded(
+              child: Text(
+                title,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
             ),
-          ),
-          const Icon(
-            Icons.chevron_right,
-            color: AppTheme.primary,
-          )
-        ],
+            const Icon(
+              Icons.chevron_right,
+              color: AppTheme.primary,
+            )
+          ],
+        ),
       ),
     );
   }

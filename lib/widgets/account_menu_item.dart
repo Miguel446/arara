71991edd2c5c1359aa564e76b149df043c907c8b
José Azevedo <joseazevedo47@hotmail.com>
2023-10-08 21:16:46 -0300

    refactor: use decoration to display divider instead of actual Divider
    Since the Divider widget has a default padding to it, it was easier to control spacing between tiles without Dividers

--- lib/widgets/account_menu_item.dart
+++ lib/widgets/account_menu_item.dart
@@ -10,38 +10,35 @@ class AccountMenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 5),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Icon(
-                  iconData,
-                  size: 22,
-                ),
-              ),
-              Expanded(
-                child: Text(
-                  title,
-                  style: const TextStyle(fontWeight: FontWeight.w500),
-                ),
-              ),
-              const Icon(
-                Icons.chevron_right,
-                color: AppTheme.primary,
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          const Divider(
-            thickness: 1,
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
             color: Colors.grey,
+            width: 1,
           ),
+        ),
+      ),
+      padding: const EdgeInsets.only(bottom: 8, top: 16),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              iconData,
+              size: 22,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ),
+          const Icon(
+            Icons.chevron_right,
+            color: AppTheme.primary,
+          )
         ],
       ),
     );

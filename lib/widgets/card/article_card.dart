565871b67155b8a734724a3119f0d170e21fb8ab
José Azevedo <joseazevedo47@hotmail.com>
2023-10-28 16:18:49 -0300

    feat(ArticleCard): open article urls on tap

--- lib/widgets/card/article_card.dart
+++ lib/widgets/card/article_card.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../config/theme.dart';
 import '../../models/article.dart';
+import '../dialogs/dialog.dart';
 import '../scaled_icon.dart';
 
 class ArticleCard extends StatelessWidget {
@@ -11,38 +14,56 @@ class ArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Article(:title, :year, :source, :author) = article;
+    final Article(:title, :year, :source, :author, :url) = article;
     final textTheme = Theme.of(context).textTheme;
 
-    return Container(
-      margin: const EdgeInsets.only(top: 24),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  title,
-                  style: textTheme.labelMedium,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  [year, source, author].join(' | '),
-                  style: textTheme.bodySmall?.copyWith(color: Colors.grey),
-                )
-              ],
+    return InkWell(
+      onTap: () async {
+        final errorDialog = AppDialog.simple(
+          context: context,
+          title: 'Erro',
+          text: 'Não foi possível abrir o link do artigo',
+          onButtonPressed: context.pop,
+        );
+
+        final uri = Uri.parse(url);
+
+        if (await canLaunchUrl(uri)) {
+          await launchUrl(uri);
+        } else {
+          errorDialog.show();
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 24),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    title,
+                    style: textTheme.labelMedium,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    [year, source, author].join(' | '),
+                    style: textTheme.bodySmall?.copyWith(color: Colors.grey),
+                  )
+                ],
+              ),
             ),
-          ),
-          const ScaledIcon(
-            Icons.chevron_right,
-            size: 30,
-            color: AppTheme.primary,
-          )
-        ],
+            const ScaledIcon(
+              Icons.chevron_right,
+              size: 30,
+              color: AppTheme.primary,
+            )
+          ],
+        ),
       ),
     );
   }
